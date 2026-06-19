abe3436a1e562f13b682022a483ec3d165ecce8b
Mark Kapapa <markkapapa15@gmail.com>
2026-06-19 18:50:46 -0400

    added player movement and input mappings

--- porg.gd
+++ porg.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 350.0
+const JUMP_VELOCITY = -1300.0
+var jump = true
+var jump_timer = 20
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +12,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_released("player_jump") and velocity.y < 0:
+		velocity.y *= 0.5;
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("player_left", "player_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
