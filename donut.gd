65407534e038636f0172ebd5114fa0c0afe6c0e3
Mark Kapapa <markkapapa15@gmail.com>
2026-06-22 03:15:37 -0400

    Changed donut.gd to CharacterBody2D, changed the scripts to move around and added collision layers to donuts, Changed sprite to have transparent background.

--- donut.gd
+++ donut.gd
@@ -1,20 +1,44 @@
-extends Area2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SPEED = 120.0
+
+var direction = 1;
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		if collision.get_normal().x > 0.5 or collision.get_normal().x < -0.5:
+			if direction == 1 and collision.get_normal().x < -0.5:
+				direction = -1
+				break
+			elif direction == -1 and collision.get_normal().x > 0.5:
+				direction = 1
+				break	
+		else:
+			pass
+			
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		if body.has_method("power_up"):
-			body.power_up(1)
-		queue_free()	
+		if body.name == "Porg":
+			if body.has_method("power_up"):
+				body.power_up(1)
+			queue_free()
 	else:
 		pass
